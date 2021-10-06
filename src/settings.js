c0e9acf9d17860355a3ed52d48c5d6f96dd9de63
Rohit Naik <rohit24naik@gmail.com>
2021-10-07 01:53:21 +0530

    second commit

--- src/settings.js
+++ src/settings.js
@@ -37,6 +37,6 @@ export const slideSettings = {
 };
 
 export const apiURL =
-  'http://api.openweathermap.org/data/2.5/forecast?q=Munich,de&APPID=75f972b80e26f14fe6c920aa6a85ad57&cnt=40';
+  'https://api.openweathermap.org/data/2.5/forecast?q=Munich,de&APPID=75f972b80e26f14fe6c920aa6a85ad57&cnt=40';
 
 export const apiURL2 = '/data.json';
